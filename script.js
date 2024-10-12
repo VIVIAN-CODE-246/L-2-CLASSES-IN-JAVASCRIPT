43aba332041e21d21590cdd5e9f9383e22abefc3
VIVIAN-CODE-246 <zaraconcept247@gmail.com>
2024-10-12 14:07:20 +0300

    CLASSES

---
+++ script.js
@@ -0,0 +1,18 @@
+class subject{
+    constructor(name, content){
+        this.name = name
+        this.content = content
+    }
+}
+const subject1 = new subject ("Biology", "Plants and Animals")
+
+console.log(subject1.name)
+console.log(subject1.content)
+
+const subject2 = new subject ("Chemistry", "Composition of Matter")
+console.log(subject2.name)
+console.log(subject2.content)
+
+const subject3 = new subject("Physics", "Energy")
+console.log(subject3.name)
+console.log(subject3.content)
